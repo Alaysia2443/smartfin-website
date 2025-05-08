5102158fef8ab85ae307c441022bb3313bbeece0
Aima Alakhume <aimailene@gmail.com>
2025-05-07 22:03:39 -0400

    update auth module declarations

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,3 @@
 import NextAuth from 'next-auth';
-import { authOptions } from './app/auth/[...nextauth]/route.ts';
+import { authOptions } from '@/app/auth/[...nextauth]/route.js';
 export const { auth, signIn, signOut } = NextAuth(authOptions);
